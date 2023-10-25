8a5d6cfd1ce6c2adcdb902bf2f84e2c14c69c411
Samuel Cho <chos5@nku.edu>
2023-10-24 23:41:11 -0400

    command.py updated

--- code/design_patterns/behavioral/command/command.py
+++ code/design_patterns/behavioral/command/command.py
@@ -1,46 +1,61 @@
 # interface
 class Command(object):
-  def execute(self): pass
+    def execute(self): pass
+
 
 class Receiver(object):
-  def __str__(self):
-    return "editor"
-  def paste(self):
-    print("Doing the paste action!")
-  def copy(self):
-    print("Doing the copy action!")
-  
-class Invoker(object): 
-  def set_command(self, command):
-    self.command = command
+    def paste(self): pass
+    def copy(self): pass
 
-# Implementations 
-class CopyCommand(Command):  
-  def execute(self, receiver, params):
-    print(f"To ({receiver}): copy {params}")    
-    receiver.copy()
 
-class PasteCommand(Command):    
-  def execute(self, receiver, params):
-    print(f"To ({receiver}): copy {params}")
-    receiver.paste()
+class Invoker(object):
+    def set_command(self, command): pass
 
-class Button(Invoker): pass
-class Editor(Receiver): pass
+# Implementations
 
-# Driver  
-## Invoker (button) initiating a request (command) to a receiver (editor)
+
+class CopyCommand(Command):
+    def execute(self, receiver, params):
+        print(f"To ({receiver}): copy {params}")
+        receiver.copy()
+
+
+class PasteCommand(Command):
+    def execute(self, receiver, params):
+        print(f"To ({receiver}): copy {params}")
+        receiver.paste()
+
+
+class Button(Invoker):
+    def set_command(self, command):
+        self.command = command
+
+
+class Editor(Receiver):
+    def __str__(self):
+        return "editor"
+
+    def paste(self):
+        print("Doing the paste action!")
+
+    def copy(self):
+        print("Doing the copy action!")
+
+
+# Driver
+# Invoker (button) initiating a request (command) to a receiver (editor)
 button1 = Button()
 button2 = Button()
 receiver = Editor()
 
 copy = CopyCommand()
-button1.set_command(copy) # when button is clicked, copy command is created
+button1.set_command(copy)  # when button is clicked, copy command is created
 
 paste = PasteCommand()
 button2.set_command(paste)
 
-## copy line 1
-copy.execute(receiver, 'line 1') # copy command sends a command to the receiver
-## paste
+# copy line 1
+# copy command sends a command to the receiver
+copy.execute(receiver, 'line 1')
+# paste
 paste.execute(receiver, 'line 3')
